b13a762a177b61d5f95ad89d0209bc32d68af7af
gxgwyj <498868779@qq.com>
2018-07-08 19:33:46 +0800

    添加了初始类

---
+++ design-pattern-factoryMethod/src/main/java/com/xyz/designpattern/factory/Factory.java
@@ -0,0 +1,10 @@
+package com.xyz.designpattern.factory;
+
+/**
+ * 类: Factory <br>
+ * 描述: ${DESCRIPTION}<br>
+ * 作者: gaoxugang <br>
+ * 时间: 2018年07月08日 19:31
+ */
+public class Factory {
+}
---
+++ design-pattern-singleton/src/main/java/com/xyz/designpattern/singleton/Singleton.java
@@ -0,0 +1,10 @@
+package com.xyz.designpattern.singleton;
+
+/**
+ * 类: Singleton <br>
+ * 描述: ${DESCRIPTION}<br>
+ * 作者: gaoxugang <br>
+ * 时间: 2018年07月08日 19:32
+ */
+public class Singleton {
+}
