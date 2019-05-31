bdd0514082042f41a71ec0fef914f0a5c10bfa18
gxgwyj <498868779@qq.com>
2019-05-31 16:08:02 +0800

    PM:观察者模式1

--- oozinoz-code/src/main/java/com/oozinoz/testing/DollarsTest.java
+++ oozinoz-code/src/main/java/com/oozinoz/testing/DollarsTest.java
@@ -11,9 +11,10 @@ package com.oozinoz.testing;
  * restriction that you may not claim that you wrote it.
  */
 
-import junit.framework.TestCase;
 
 import com.oozinoz.utility.Dollars;
+import junit.framework.TestCase;
+
 
 public class DollarsTest extends TestCase {
     public void testZero() {
